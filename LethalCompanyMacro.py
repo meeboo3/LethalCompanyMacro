24c9e31152b64254743e83125bb144a279c5b28d
meeboo3 <118372959+meeboo3@users.noreply.github.com>
2024-01-15 17:38:07 +0800

    Update LethalCompanyMacro.py

--- LethalCompanyMacro.py
+++ LethalCompanyMacro.py
@@ -5,27 +5,34 @@ import win32con
 import keyboard
 import pyperclip
 
+version = '1.2.0'
+
 if os.path.exists('LCM.txt'):
     with open('LCM.txt', encoding='utf-8', mode='rt') as f:
-        switch_key, transmit_key = f.read().split(' ')
+        switch_key, transmit_key = [i.replace('_', ' ').split('+') for i in f.read().split(' ')]
 else:
-    switch_key = 'pagedown'
-    transmit_key = 'down'
+    switch_key = ['page down']
+    transmit_key = ['down']
 
 curr_copy = ''
 run = False
 
 
-def run_wrapper(fn):
-    def _(args):
-        global run
-        if run:
-            return
-        run = True
-        fn(args)
-        run = False
+def check(keys: list[str]):
+    def fn(event: keyboard.KeyboardEvent):
+        if not event.name == keys[-1]:
+            return False
+        for key in keys[:-1]:
+            if not keyboard.is_pressed(key):
+                return False
+        else:
+            return True
 
-    return _
+    return fn
+
+
+switch_check = check(switch_key)
+transmit_check = check(transmit_key)
 
 
 def log(text):
@@ -49,8 +56,7 @@ def paste():
     win32api.keybd_event(17, 0, win32con.KEYEVENTF_KEYUP, 0)
 
 
-@run_wrapper
-def switch(evt):
+def switch():
     copy('switch')
     paste()
     # enter
@@ -59,8 +65,7 @@ def switch(evt):
     log('enter switch')
 
 
-@run_wrapper
-def transmit(evt):
+def transmit():
     copy('transmit ')
     paste()
     log('enter transmit')
@@ -73,9 +78,21 @@ text_key = {
     'left': '←'
 }
 
-print(f'press `{text_key.get(switch_key) or switch_key}` to quickly enter `switch + enter`\n'
-      f'press `{text_key.get(transmit_key) or transmit_key}` to quickly enter `transmit `\n'
-      'https://github.com/meeboo3/LethalCompanyMacro v1.1.0')
-keyboard.on_press_key(switch_key, switch)
-keyboard.on_press_key(transmit_key, transmit)
+
+def fmt(text: list[str]):
+    return ' + '.join(text_key.get(i, i) for i in text)
+
+
+def process(event):
+    if switch_check(event):
+        switch()
+    elif transmit_check(event):
+        transmit()
+
+
+print(f'press `{fmt(switch_key)}` to quickly enter `switch + enter`\n'
+      f'press `{fmt(transmit_key)}` to quickly enter `transmit `\n'
+      f'https://github.com/meeboo3/LethalCompanyMacro v{version}')
+
+keyboard.on_press(process)
 keyboard.wait()
